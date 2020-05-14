793c8b15df3e31749ce096c01c9de0043ec5d471
Theo Ottah <theo@openfin.co>
2020-05-14 17:43:11 +0100

    Updates

---
+++ precache-manifest.6720824aeb2185a56557a22a75ead238.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "841b2a22128a2ca111eaa3df18de5ed8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "07d5c60fcbb17f56fc8a",
+    "url": "./static/css/2.75cc5a33.chunk.css"
+  },
+  {
+    "revision": "a7b99b15e9e6d7a7d50b",
+    "url": "./static/css/main.e18d2753.chunk.css"
+  },
+  {
+    "revision": "07d5c60fcbb17f56fc8a",
+    "url": "./static/js/2.e1163ec8.chunk.js"
+  },
+  {
+    "revision": "2cd639bd6a08c4dc941f80b900c32d70",
+    "url": "./static/js/2.e1163ec8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7b99b15e9e6d7a7d50b",
+    "url": "./static/js/main.658119ca.chunk.js"
+  },
+  {
+    "revision": "f2b1d98e2d4db917c6fd",
+    "url": "./static/js/runtime-main.b9882bb2.js"
+  }
+]);
